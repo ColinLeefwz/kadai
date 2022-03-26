7047fee049d5c323ec8007d14388480914d35463
ColinLeefwz <lilonglongfwz@gmail.com>
2022-03-26 21:25:46 +0900

    Create images upload page (#12)
    * implement images uplaoding
    
    - enable active storage
    - implement saving images
    - add scss to image page
    - add validations
    - add I18n config
    - add file uplaoding test cases
    
    * [bug] fix bugs
    
    Co-authored-by: lolo lee <lololee@lolos-MacBook-Pro-2.local>

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    let(:blank_title_msg) { 'タイトルタイトルを入力してください' }
+    let(:too_long_title_msg) { 'タイトルは30文字以内で入力してください' }
+    let(:blank_avatar_msg) { '画像ファイル画像ファイルを入力してください' }
+
+    it 'is invalid without a title' do
+      image = described_class.new
+      image.valid?
+      expect(image.errors.full_messages).to match_array([blank_title_msg, blank_avatar_msg])
+    end
+
+    it 'is invalid without a avatar' do
+      image = described_class.new(title: 'title')
+      image.valid?
+      expect(image.errors.full_messages).to match_array([blank_avatar_msg])
+    end
+
+    it 'is invalid with a too long title' do
+      image = described_class.new(title: 'title' * 20, avatar: 'avatar')
+      image.valid?
+      expect(image.errors.full_messages).to match_array([too_long_title_msg])
+    end
+  end
 end
